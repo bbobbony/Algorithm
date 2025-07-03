9cd383b9dddc3c886616be2698a9967c2208bc1a
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-07-03 23:50:50 +0900

    Update 두 수의 연산값 비교하기.java

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -11,3 +11,16 @@ class Solution {
         return Integer.parseInt(special) > normal ? Integer.parseInt(special) : normal ;
     }
 }
+
+
+//다른사람의 인상깊은 코드 메모해두기
+class Solution {
+    public int solution(int a, int b) {
+        int ab = Integer.parseInt(a + "" + b);
+        if(2 * a * b <= ab) {
+            return ab;
+        } else {
+            return 2 * a * b;
+        }
+    }
+}
