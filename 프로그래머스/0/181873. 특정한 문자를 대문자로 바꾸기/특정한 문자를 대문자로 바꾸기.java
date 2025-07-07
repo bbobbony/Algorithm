25b4171bc0d410e9c9def9d60812b950ba7b6303
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-07-07 18:28:43 +0900

    Update 특정한 문자를 대문자로 바꾸기.java

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -14,3 +14,15 @@ class Solution {
         return result;
     }
 }
+
+
+//다른 사람이 작성한 코드
+class Solution {
+    public String solution(String my_string, String alp) {
+        String a = alp.toUpperCase();
+        return my_string.replaceAll( alp, a);
+    }
+}
+
+// alp를 대문자로 바꿔두고
+//replaceAll함수를 사용해 my_string에서 alp문자를 a 즉 대문자로 교체해준다
