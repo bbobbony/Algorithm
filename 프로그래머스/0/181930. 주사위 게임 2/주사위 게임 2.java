d3195ec63b0b78ce97f7697fcb17b796798153d2
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-07-09 01:34:14 +0900

    Update 주사위 게임 2.java

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -11,3 +11,30 @@ class Solution {
         return answer;
     }
 }
+
+//또다른 풀이(재귀함수를 이용해서 제곱부분을 함수화)
+class Solution{
+    public int solution(int a, int b, int c){
+        int answer = 1;
+        int count =1
+        if(a==b && b==c){
+            count++;
+        }
+        if(a==b || b==a || c==a){
+            count++;
+        }
+
+        for(int i=1; i<=count; i++){
+            answer *= (pow(a, i) + pow(b, i) + pow(c, i));
+        }
+
+        return answer;
+    }
+
+    private int pow(int a, int b){
+        if(b == 0){
+            retunr 1;
+        }
+        return a * pow(a, b-1);
+    }
+}
