2b15f24ed4f624b61a1347a8a7b4f9e4b5687f57
kangboeun <142016790+bbobbony@users.noreply.github.com>
2025-09-03 00:16:17 +0900

    Update 코드 처리하기.java

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -19,5 +19,26 @@ class Solution {
 
         // ret이 비어있으면 "EMPTY" 반환
         return ret.length() == 0 ? "EMPTY" : ret.toString();
+
+        
+//다른사람이 작성한 코드
+class Solution {
+    public String solution(String code) {
+        StringBuilder answer = new StringBuilder();
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0; //삼항연산자로 가독성 있게
+                continue;
+            }
+
+            if (i % 2 == mode) { 
+                answer.append(current);
+            } 
+        }
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
+    }
+}
     }
 }
